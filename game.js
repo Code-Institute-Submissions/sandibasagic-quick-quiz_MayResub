8979e083e54e2264756b1be9ed74f7edc5add640
sandibasagic <sandi.basagic@gmail.com>
2022-03-03 17:00:32 +0000

    Added JS toggle function for hiding sectionHome

---
+++ game.js
@@ -0,0 +1,9 @@
+function toggle() {
+    const sectionHome = document.getElementById('sectionHome');
+    if(sectionHome.style.display === "none") {
+        sectionHome.style.display = "block";
+    }
+    else {
+        sectionHome.style.display = "none";
+    }
+};
