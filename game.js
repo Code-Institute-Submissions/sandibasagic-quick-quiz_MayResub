ff3d9a63537b375d43859795e435b35908c05a11
sandibasagic <sandi.basagic@gmail.com>
2022-03-03 18:20:15 +0000

    Assigning variables of questions,answer,score,counter

--- game.js
+++ game.js
@@ -9,4 +9,8 @@ function toggle() {
 };
 const question = document.getElementById('question')
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availiableQuestions = [];
