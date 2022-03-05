9ea44d8eaefc53fd4723c72b575cfe145d0e79cc
sandibasagic <sandi.basagic@gmail.com>
2022-03-05 08:27:49 +0000

    Cleaning and fixing code with classToApply

--- game.js
+++ game.js
@@ -97,16 +97,20 @@ getNewQuestion = () => {
     availiableQuestions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
-choices.forEach (choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+        const selectedAnswer = selectedChoice.dataset["number"];
+
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        
         selectedChoice.parentElement.classList.add(classToApply);
         selectedChoice.parentElement.classList.remove(classToApply);
-        getNewQuestion();
+    
+    getNewQuestion();
     })
 })
 startGame();
