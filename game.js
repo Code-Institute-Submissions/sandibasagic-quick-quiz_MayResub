4d88bdb584bf188efc9fbdc9ca6d45b7fdf0fe2c
sandibasagic <sandi.basagic@gmail.com>
2022-03-06 15:27:38 +0000

    Code formated and cleaned

--- game.js
+++ game.js
@@ -17,27 +17,28 @@ function toggle(event) {
     // Show Section
     gameSectionRef.classList.remove("hidden");
   }
-  
+
   // console.log(event.target.classList);
   else if (event.target.id === "startAgain") {
     // Hide Section
     gameSectionRef.classList.add("hidden");
     // Show Section
-    endSectionRef.classList.remove("hidden")}
-else if (event.target.id === "finishGame") {
+    endSectionRef.classList.remove("hidden");
+  } else if (event.target.id === "finishGame") {
     // Hide Section
     endSectionRef.classList.add("hidden");
     // Show Section
-    introSectionRef.classList.remove("hidden")};
-};
+    introSectionRef.classList.remove("hidden");
+  }
+}
 // Heads up display counter
 
-const questionCounterText = document.getElementById('questionCounter');
-const scoreText = document.getElementById('score');
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 // Questions var
-const question = document.getElementById('question')
-const choices = Array.from(document.getElementsByClassName('choice-text'));
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -45,30 +46,30 @@ let questionCounter = 0;
 let availiableQuestions = [];
 
 let questions = [
-    {
-        question: "What day is today?",
-        choice1: "Monday",
-        choice2: "Friday",
-        choice3: "Sunday",
-        choice4: "Tuesday",
-        answer: 1
-    },
-    {
-        question: "When does the christmas come?",
-        choice1: "January",
-        choice2: "March",
-        choice3: "December",
-        choice4: "August",
-        answer: 3
-    },
-    {
-        question: "What is the capital city of England?",
-        choice1: "Manchester",
-        choice2: "Liverpool",
-        choice3: "Newcastle",
-        choice4: "London",
-        answer: 4
-    }
+  {
+    question: "What day is today?",
+    choice1: "Monday",
+    choice2: "Friday",
+    choice3: "Sunday",
+    choice4: "Tuesday",
+    answer: 1,
+  },
+  {
+    question: "When does the christmas come?",
+    choice1: "January",
+    choice2: "March",
+    choice3: "December",
+    choice4: "August",
+    answer: 3,
+  },
+  {
+    question: "What is the capital city of England?",
+    choice1: "Manchester",
+    choice2: "Liverpool",
+    choice3: "Newcastle",
+    choice4: "London",
+    answer: 4,
+  },
 ];
 
 // Constants
@@ -77,53 +78,54 @@ const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    availiableQuestions = [...questions];
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availiableQuestions = [...questions];
+  getNewQuestion();
 };
 getNewQuestion = () => {
-    if(availiableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        // Go to the end page SECTION!
-        gameSectionRef.classList.add("hidden");
-        // Show Section
-        endSectionRef.classList.remove("hidden");
-        return
-    }
-    questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`
-    const questionIndex = Math.floor(Math.random() * availiableQuestions.length)
-    currentQuestion = availiableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-    choices.forEach( choice => {
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number];
-    });
-    availiableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+  if (availiableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    // Go to the end page SECTION!
+    gameSectionRef.classList.add("hidden");
+    // Show Section
+    endSectionRef.classList.remove("hidden");
+    return;
+  }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+  const questionIndex = Math.floor(Math.random() * availiableQuestions.length);
+  currentQuestion = availiableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+  availiableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers) return;
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
 
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        if(classToApply === 'correct') {
-            incrementScore(CORRECT_BONUS);
-        }
-        
-        selectedChoice.parentElement.classList.add(classToApply);
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 800);
-    });
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 800);
+  });
 });
-incrementScore = num => {
-    score +=num;
-    scoreText.innerText = score;
-}
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 startGame();
